489d3e89d290de9cce4a5deecda3fcac1bfd25e5
Shifat507 <ferdoushasanshifat@gmail.com>
2025-01-24 21:07:49 +0600

    update home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <h1 className="text-3xl font-bold">Your Daily Dose of Knowledge</h1>
                 
             
-                <p className="mt-4">Dive into a world of fresh perspectives and endless possibilities. Our blog is your go-to destination for trending topics, expert opinions, and thought-provoking insights. Whether you're looking to explore the latest in technology, lifestyle, business, or personal growth, we've got you covered. Each post is carefully curated to keep you informed, inspired, and ready to take on the world with new ideas. Stay curious, stay engaged, and let our content be your daily source of inspiration and knowledge.</p>
+                <p className="mt-4">Dive into a world of fresh perspectives and endless possibilities. Our blog is your go-to destination for trending topics, expert opinions, and thought-provoking insights. Whether you’re looking to explore the latest in technology, lifestyle, business, or personal growth, we’ve got you covered. Each post is carefully curated to keep you informed, inspired, and ready to take on the world with new ideas. Stay curious, stay engaged, and let our content be your daily source of inspiration and knowledge.</p>
 
                 <Link href='/blogs' className="btn btn-primary mt-16">See All Blogs</Link>
             </div>
